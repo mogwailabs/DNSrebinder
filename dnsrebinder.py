87d5be644bc8249e6f3d4cbab38b4c576ec6f79f
Gabriel Schneider <gabrielschneider100@gmail.com>
2023-05-15 16:32:04 -0300

    cleanup

--- dnsrebinder.py
+++ dnsrebinder.py
@@ -45,13 +45,11 @@ class Colors:
     NEGATIVE = "\033[7m"
     CROSSED = "\033[9m"
     END = "\033[0m"
-    # cancel SGR codes if we don't write to a terminal
     if not __import__("sys").stdout.isatty():
         for _ in dir():
             if isinstance(_, str) and _[0] != "_":
                 locals()[_] = ""
     else:
-        # set Windows console in VT mode
         if __import__("platform").system() == "Windows":
             kernel32 = __import__("ctypes").windll.kernel32
             kernel32.SetConsoleMode(kernel32.GetStdHandle(-11), 7)
@@ -103,8 +101,6 @@ def dns_response(data, domain, ip, rebind, ttl, counterMax, hostCounter, client_
     request = DNSRecord.parse(data)
     global last_time
 
-    # print(request)
-
     reply = DNSRecord(DNSHeader(id=request.header.id, qr=1, aa=1, ra=1), q=request.q)
 
     qname = request.q.qname
@@ -116,17 +112,15 @@ def dns_response(data, domain, ip, rebind, ttl, counterMax, hostCounter, client_
 
         now = datetime.datetime.now()
 
+        # Add a newline if the request has taken some time
         if last_time != None and (now - last_time).total_seconds() > 3:
             print('')
 
         last_time = now
 
-
-        #print(request)
         rqt = "A"
         ctime = time_fmt()
         if qt in ['*', rqt]:
-            #print("request for " + str(qname) + " Type: " + str(qt))
             if qn in hostCounter:
                 if should_rebind(client_ip, hostCounter[qn], counterMax):
                     reply.add_answer(RR(rname=qname, rtype=getattr(QTYPE, rqt), rclass=1, ttl=ttl, rdata=A(ip)))
@@ -153,11 +147,9 @@ class BaseRequestHandler(socketserver.BaseRequestHandler):
 
     def handle(self):
         now = datetime.datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S.%f')
-        #print("\n\n%s request %s (%s %s):" % (self.__class__.__name__[:3], now, self.client_address[0],
-        #                                       self.client_address[1]))
+        
         try:
             data = self.get_data()
-        #   print(len(data), data)  # repr(data).replace('\\x', '')[1:-1]
             self.send_data(dns_response(data, self.server.domain, self.server.ip, self.server.rebind, self.server.ttl, self.server.counterMax, self.server.hostCounter, self.client_address[0]))
         except Exception:
             traceback.print_exc(file=sys.stderr)
@@ -166,7 +158,6 @@ class BaseRequestHandler(socketserver.BaseRequestHandler):
 class TCPRequestHandler(BaseRequestHandler):
 
     def get_data(self):
-        #print(f'> {self.client_address[0]}')
         data = self.request.recv(8192)
         sz = struct.unpack('>H', data[:2])[0]
         if sz < len(data) - 2:
@@ -183,7 +174,6 @@ class TCPRequestHandler(BaseRequestHandler):
 class UDPRequestHandler(BaseRequestHandler):
 
     def get_data(self):
-        #print(f'{Colors.CYAN}> {self.client_address[0]}{Colors.END}')
         return self.request[0]
 
     def send_data(self, data):
@@ -206,7 +196,7 @@ def main():
     args = parser.parse_args()
     if not (args.udp or args.tcp): parser.error("Please select at least one of --udp or --tcp.")
 
-    #print("Starting nameserver...")
+    print("Starting nameserver...\n")
 
     servers = []
     if args.udp: servers.append(socketserver.ThreadingUDPServer((args.bind, args.port), UDPRequestHandler))
@@ -223,7 +213,6 @@ def main():
         thread = threading.Thread(target=s.serve_forever)  # that thread will start one more thread for each request
         thread.daemon = True  # exit the server thread when the main thread terminates
         thread.start()
-        #print("%s server loop running in thread: %s" % (s.RequestHandlerClass.__name__[:3], thread.name))
 
     try:
         while 1:
